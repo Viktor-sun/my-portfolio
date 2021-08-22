f734c58d2005e82d347ebd73d299312d8bdc4c03
Viktor-sun <bobick.zdoh@gmail.com>
2021-08-22 17:20:30 +0300

    add about page

--- src/App.js
+++ src/App.js
@@ -37,19 +37,6 @@ export default function App() {
         </Switch>
       </Suspense>
       <Footer />
-      {/* <span style={{ fontSize: '60px' }}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero tempora
-        repellat repudiandae, reprehenderit cupiditate, id explicabo corrupti
-        hic, at iusto fugit assumenda sit esse laudantium. Repellendus debitis
-        rerum deleniti sunt soluta impedit possimus odio, quis hic beatae.
-        Minima quod assumenda fuga architecto optio, cumque, ratione qui vel
-        modi aut fugiat animi tenetur aperiam, amet dolores! Porro consectetur
-        quasi fugiat accusamus incidunt, inventore, voluptatem quas eius
-        consequatur adipisci deserunt maxime illum neque dolores praesentium ab!
-        Harum inventore tempora iusto molestiae officiis maxime, eum nihil nisi
-        fugit cupiditate quisquam voluptas ipsam ut in, quis deserunt
-        reprehenderit soluta facilis nesciunt corrupti. Ea, reprehenderit!
-      </span> */}
     </div>
   );
 }
--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 import Background from '../../components/Background';
+import styles from './AboutPage.module.css';
+import photo from '../../assets/images/photo2.jpg';
 
 export default function AboutPage() {
   return (
-    <Background>
-      <div>AboutPage</div>
-    </Background>
+    <>
+      <Background height="300px">
+        <Typography variant="h1" className={styles.title}>
+          Some About Me
+        </Typography>
+      </Background>
+      <section className={styles.section}>
+        <img src={photo} alt="my portrait" className={styles.img} />
+
+        <div className={styles.aboutWrapper}>
+          <p className={styles.aboutTitle}>
+            <strong>Hi!</strong>
+          </p>
+
+          <p className={styles.aboutDescription}>
+            I'm Viktor located in Kharkiv, Ukraine. I really like to program,
+            make complex tasks simple and to help people with this. I like
+            solving design problems, create smart user interface. I want and
+            love to develop, studies something new. When I don't click on the
+            pixels, you can find me on the street while cycling or near the
+            tennis table.
+          </p>
+        </div>
+      </section>
+      <Background height="70px" />
+    </>
   );
 }
